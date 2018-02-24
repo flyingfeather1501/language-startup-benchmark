6bcbbbbfb4ef5f1e6c6b6afb025a83b26b9850c5
Kisaragi Hiu <mail@kisaragi-hiu.com>
2018-02-24 17:48:40 +0900

    language -> title

--- report.bash
+++ report.bash
@@ -2,9 +2,9 @@
 # report functions
 # this script can be used on its own as the report_time function
 report_success () {
-    # report_success <language> <output> <time>
+    # report_success <title> <output> <time>
     cat <<JSON
-{"status":"success","report":{"language":"$1","output":"$2","time":$3,"unit":"second"}}
+{"status":"success","report":{"title":"$1","output":"$2","time":$3,"unit":"second"}}
 JSON
 }
 
@@ -16,9 +16,9 @@ JSON
 
 # time the command and format the result
 report_time () {
-    # report_time <language> <command> <args> ...
+    # report_time <title> <command> <args> ...
     TIMEFORMAT="%3R"
-    language="$1"; shift 1
+    title="$1"; shift 1
     tmp_time=$(mktemp)
     tmp_cmd_output=$(mktemp)
 
@@ -31,7 +31,7 @@ report_time () {
 
     time_seconds=$(cat "$tmp_time")
     output=$(cat "$tmp_cmd_output")
-    report_success "$language" "$output" "$time_seconds"
+    report_success "$title" "$output" "$time_seconds"
     rm "$tmp_time" "$tmp_cmd_output"
 }
 
